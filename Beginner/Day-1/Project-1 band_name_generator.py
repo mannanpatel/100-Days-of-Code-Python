4dd3e0d88fe3ac60b43f7ef2a7d4ffeb3a961d3c
Manan Patel <106650519+mannanpatel@users.noreply.github.com>
2023-01-11 01:57:08 +0530

    Add files via upload

---
+++ Beginner/Day-1/Project-1 band_name_generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Welcome to brand name Generator")
+#2. Ask the user for the city that they grew up in.
+city_name = input("Enter city name were you grew up? \n")
+#3. Ask the user for the name of a pet.
+pet_name = input("Enter your pet name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your Brand Name could be: " + city_name + " " + pet_name)
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
